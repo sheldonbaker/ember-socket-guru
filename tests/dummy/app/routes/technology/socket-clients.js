2dc872776165f28c5d38c819532a6433338d4769
Justyn Jozwiak <justyn.jozwiak@netguru.pl>
2017-03-30 13:17:08 +0200

    socket-clients: update route

--- tests/dummy/app/routes/technology/socket-clients.js
+++ tests/dummy/app/routes/technology/socket-clients.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 const { Route, get } = Ember;
 
 export default Route.extend({
-  model() {
-    return get(this, 'routeName').split('.')[1].replace(/-/g, ' ');
+  model(params, model) {
+    return {
+      routeName: get(this, 'routeName').split('.')[1].replace(/-/g, ' '),
+      technology: model.resolvedModels.technology,
+    };
   },
 });
