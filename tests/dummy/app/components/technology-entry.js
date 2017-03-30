e70cf7215e15981487f884c9a906e18973c33ba9
Justyn Jozwiak <justyn.jozwiak@netguru.pl>
2017-03-30 23:34:36 +0200

    technology-entry: update component

--- tests/dummy/app/components/technology-entry.js
+++ tests/dummy/app/components/technology-entry.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
-const { Component, get } = Ember;
+const { Component, get, computed } = Ember;
 
 export default Component.extend({
+  isSelected: computed('selectedTechnology', function() {
+    return get(this, 'technology.name') === get(this, 'selectedTechnology.name');
+  }),
+
+  technologyClass: computed('isSelected', function() {
+    return get(this, 'isSelected') ?
+      'c-main-page__technology' :
+      'c-main-page__technology c-main-page__technology--faded';
+  }),
+
   click() {
     get(this, 'onClick')(get(this, 'technology.name'));
   },
