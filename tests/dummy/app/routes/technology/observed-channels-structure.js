bb202f1dc7cdfe36cee899f37c5d26b7ab5a9854
Justyn Jozwiak <justyn.jozwiak@netguru.pl>
2017-03-30 13:17:01 +0200

    observed-channels-structure: update route

--- tests/dummy/app/routes/technology/observed-channels-structure.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-const { Route, get } = Ember;
-
-export default Route.extend({
-  model() {
-    return get(this, 'routeName').split('.')[1].replace(/-/g, ' ');
-  },
-});
