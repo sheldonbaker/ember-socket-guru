d40f913c6bc12fdfb81609cd38d7044a28d67a87
Jacek Bandura <jacek.bandura@netguru.pl>
2017-04-25 11:02:55 +0200

    Adjust snippets

--- tests/dummy/app/snippets/events-only.js
+++ tests/dummy/app/snippets/events-only.js
@@ -1,3 +1,6 @@
-{
+import SocketGuru from 'ember-socket-guru/services/socket-guru';
+
+export default SocketGuru.extend({
+  // other config goes here ...
   observedChannels: ['event1', 'event2'],
-}
+});
--- tests/dummy/app/snippets/observed-channels.js
+++ tests/dummy/app/snippets/observed-channels.js
@@ -1,5 +1,6 @@
-{
-  observedChannels: {
-    channel1: ['event1', 'event2'],
-  }
-}
+import SocketGuru from 'ember-socket-guru/services/socket-guru';
+
+export default SocketGuru.extend({
+  // other config goes here ...
+  observedChannels: ['event1', 'event2'],
+});
