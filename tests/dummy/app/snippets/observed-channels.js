613c563b07064ef0b677e684f8addb396cdb941f
Jacek Bandura <jacek.bandura@netguru.pl>
2017-06-03 19:59:04 +0200

    Update the observedChannels docs

--- tests/dummy/app/snippets/observed-channels.js
+++ tests/dummy/app/snippets/observed-channels.js
@@ -2,5 +2,7 @@ import SocketGuru from 'ember-socket-guru/services/socket-guru';
 
 export default SocketGuru.extend({
   // other config goes here ...
-  observedChannels: ['event1', 'event2'],
+  observedChannels: {
+    channel1: ['event1', 'event2'],
+  },
 });
